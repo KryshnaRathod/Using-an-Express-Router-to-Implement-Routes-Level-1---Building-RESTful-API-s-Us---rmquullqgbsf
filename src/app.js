17e53f09cca31994c0c4dfc392da10bf4be5fa36
Krishna Rathod <88035445+KryshnaRathod@users.noreply.github.com>
2023-07-23 23:03:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,86 +1,69 @@
 const fs = require('fs');
 const express = require('express');
-const { object } = require('joi');
 const app = express();
-const router = new express.Router();
-const bodyParser = require('body-parser');
+//Aim: With the help of router, get all the product with router.GET request and create a product with router.POST request
 
 //middleware
-router.use(express.json());
-router.use(bodyParser.json());
+app.use(express.json());
 
+//write router middleware here
+const router = express.Router();
+app.use(router);
+
+//Including product.json file
 const product = JSON.parse(
-  fs.readFileSync(`${__dirname}/../dev-data/product.json`)
+	fs.readFileSync(`${__dirname}/../dev-data/product.json`)
 );
 
 // Defining The Router
-// Handling PATCH request
-router.patch('/api/v1/product/:id', (req, res) => {
-  try {
-    //Write your code here
-    const id = req.params.id;
-    const productIndex = product.findIndex((p) => p.id == id);
-    if (productIndex === -1) {
-      return res.status(404).json({
-        message: 'Product Not Found',
-        status: 'Error',
-      });
-    }
-    product[productIndex] = Object.assign(product[productIndex], req.body);
-    fs.writeFileSync(
-      `${__dirname}/../dev-data/product.json`,
-      JSON.stringify(product)
-    );
-    return res.status(201).json({
-      message: 'success',
-      data: {
-        product,
-      },
-    });
-    
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      message: 'Product Updation Failed',
-      status: 'Error',
-    });
-  }
+// Get all the products
+router.get('/api/v1/product', (req, res) => {
+	try {
+		//Write your code here
+		res.status(200).json({
+			status: 'success',
+			results: product.length,
+			data: { product },
+		});
+	} catch (error) {
+		res.status(400).json(error);
+	}
 });
 
-//Deleting Product
-router.delete('/api/v1/product/:id', (req, res) => {
-  try {
-    //Write your code here
-     const id = req.params.id;
-    const productIndex = product.findIndex((p) => p.id == id);
-    if (productIndex === -1) {
-      return res.status(404).json({
-        message: 'Product Not Found',
-        status: 'Error',
-      });
-    }
-    product.splice(productIndex, 1);
-    fs.writeFileSync(
-      `${__dirname}/../dev-data/product.json`,
-      JSON.stringify(product)
-    );
-    return res.status(201).json({
-      status: 'success',
-      data: {
-        product,
-      },
-    });
-    
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({
-      message: 'Product Deletion Failed',
-      status: 'Error',
-    });
-  }
+//Create a new Product
+router.post('/api/v1/product', (req, res) => {
+	try {
+		//Write your code here
+		const newProduct = req.body;
+		if (!newProduct.title || !newProduct.price) {
+			return res.status(404).json({
+				status: 'Error',
+				message: 'Title and price are required',
+			});
+		}
+		const id = product[product.length - 1].id + 1;
+		newProduct.id = id;
+		const products = [...product, newProduct];
+		console.log(products);
+		fs.writeFileSync(
+			`${__dirname}/../dev-data/product.json`,
+			JSON.stringify(products)
+		);
+		res.status(201).json({
+			status: 'success',
+			data: {
+				product: newProduct,
+			},
+		});
+	} catch (error) {
+		res.status(400).json({
+			message: 'Error creating product',
+			status: 'Error',
+		});
+	}
 });
 
 //Registering our Router
-app.use(router);
+//Write here to register router
 
 module.exports = app;
